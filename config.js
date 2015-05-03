352aaeb99b8aad49caa533d6d5f54b53c7f75dad
ramanathan <ramanathan.gksvv@gmail.com>
2015-05-03 12:55:20 +0000

    added codio config

--- config.js
+++ config.js
@@ -7,6 +7,10 @@ if(process.env.PORT && process.env.C9_HOSTNAME){
 	PORT = process.env.PORT;
 	HOSTNAME = process.env.C9_HOSTNAME;
 }
+//codio Configuration (https://codio.com/)
+if(require('os') && require('os').hostname()){
+    HOSTNAME = require('os').hostname()+ '.codio.io'
+}
 
 module.exports = {
 	"PORT" : PORT,
